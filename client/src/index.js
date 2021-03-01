eced74f15f644727114f1f7896c9186c21189dd3
Michael <michaelbubel.1989@gmail.com>
2021-03-01 14:07:21 -0800

    Login and Signup components

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./style/index.css";
 import App from "./App";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById("root")
+);
+
