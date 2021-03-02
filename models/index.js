a3ea0cea6d586ed2e7255b8d8eba19da83c9c74f
anaiva27 <nastia.iv.91@gmail.com>
2021-03-01 21:48:09 -0800

    schemas added

--- models/index.js
+++ models/index.js
@@ -1,9 +1,4 @@
 // Commented out lines are from original copied file. After creating the api routes for this application copy the commented lines and update with appropriate information
-
 module.exports = {
-  // Book: require("./book")
-  // require schemas
-  // User
-  // Grocery/Fridge List
-  // Expiration Date table
+  FreshFridge: require("./FreshFridge")
 };
--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -6,19 +6,22 @@ const db = require("../models");
 
 // This file empties the Books collection and inserts the books below
 
-mongoose.connect(
-  process.env.MONGODB_URI ||
-  "mongodb://localhost/reactreadinglist"
-);
+// mongoose.connect(
+//   process.env.MONGODB_URI ||
+//   "mongodb://localhost/reactreadinglist"
+// );
 
-const bookSeed = [
+mongoose.connect("mongodb://localhost/FreshFridge", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+
+const product = [
   {
-    title: "The Dead Zone",
-    author: "Stephen King",
-    synopsis:
-      "A number-one national best seller about a man who wakes up from a five-year coma able to see people's futures and the terrible fate awaiting mankind in The Dead Zone - a \"compulsive page-turner\" (The Atlanta Journal-Constitution). Johnny Smith awakens from a five-year coma after his car accident and discovers that he can see people's futures and pasts when he touches them. Many consider his talent a gift; Johnny feels cursed. His fiancée married another man during his coma, and people clamor for him to solve their problems. When Johnny has a disturbing vision after he shakes the hand of an ambitious and amoral politician, he must decide if he should take drastic action to change the future. The Dead Zone is a \"faultlessly paced...continuously engrossing\" (Los Angeles Times) novel of second sight.",
-    date: new Date(Date.now())
-  },
+    product: "The Dead Zone",
+    shelfLife: 14,
+      },
   {
     title: "Lord of the Flies",
     author: "William Golding",
@@ -126,11 +129,11 @@ const bookSeed = [
   }
 ];
 
-db.Book
+db.FreshFridge
   .remove({})
-  .then(() => db.Book.collection.insertMany(bookSeed))
+  .then(() => db.FreshFridge.collection.insertMany(product))
   .then(data => {
-    console.log(data.result.n + " records inserted!");
+    console.log(data.result.n + " product dates have been inserted!");
     process.exit(0);
   })
   .catch(err => {
