a3fad0d362db307502627cd61b4561ecc09de7e5
Michael <michaelbubel.1989@gmail.com>
2021-03-06 13:54:49 -0800

    Basic Grocery List Style

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,41 +1,45 @@
-import React, { useState } from "react"
-import Item from "./Item"
+import React, { useState } from "react";
+import Item from "./Item";
 
 export default function List() {
+  const [notes, setNotes] = useState([]);
+  const [input, setInput] = useState("");
 
-    const [notes, setNotes] = useState([])
-    const [input, setInput] = useState("")
+  const handleSubmit = (e, notes, setNotes, input, setInput) => {
+    e.preventDefault();
+    const id = notes.length ? notes[notes.length - 1].id + 1 : 0;
+    setNotes([...notes, { id: id, message: input }]);
+    setInput("");
+  };
 
-    const handleSubmit = (e, notes, setNotes, input, setInput) => {
-        e.preventDefault()
-        const id = (notes.length) ? notes[notes.length - 1].id + 1 : 0;
-        setNotes([...notes, { id: id, message: input }])
-        setInput("")
-    }
+  const deleteNote = (id, notes, setNotes) => {
+    setNotes(notes.filter((note) => note.id !== id));
+  };
 
-    const deleteNote = (id, notes, setNotes) => {
-        setNotes(notes.filter(note => note.id !== id))
-        // setMove(notes.filter(note => note.id == id))
-    }
+  return (
+    <div className="auth-wrapper">
+      <div className="auth-inner">
+        <div>
+          <h2>Add items to your grocery list!</h2>
+          <form
+            onSubmit={(e) => handleSubmit(e, notes, setNotes, input, setInput)}
+          >
+            <input onChange={(e) => setInput(e.target.value)} value={input} />
+            <button id="addButton" className="btn btn-success">
+              ADD
+            </button>
 
-    return (
-        <div className="auth-wrapper">
-            <div className="auth-inner">
-                <div>
-                    <h2>Add items to your grocery list!</h2>
-                    <form onSubmit={(e) => handleSubmit(e, notes, setNotes, input, setInput)}>
-                        <button>Submit</button>
-                        <input onChange={e => setInput(e.target.value)} value={input} />
-                    </form>
-                    {notes.map(note => (
-                        <Item id={note.id} message={note.message} deleteNote={id => deleteNote(id, notes, setNotes)} />))}
-                    <hr />
-                    {/* {notes.map(note => (
-          <Note id={note.id} message={note.message} deleteNote={id => deleteNote(id, notes, setNotes)} />))}   */}
-                </div>
-            </div>
+            {notes.map((note) => (
+              <Item
+                id={note.id}
+                message={note.message}
+                deleteNote={(id) => deleteNote(id, notes, setNotes)}
+              />
+            ))}
+          </form>
+          <hr />
         </div>
-
-
-    );
+      </div>
+    </div>
+  );
 }
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -8,16 +8,13 @@ function SignUp() {
     email: "",
     password: "",
   });
-
   useEffect(() => {
     // not sure if anything is needed here
   }, []);
-
   function handleInputChange(e) {
     const { name, value } = e.target;
     setNewUser({ ...newUser, [name]: value });
   }
-
   function handleFormSubmit(e) {
     e.preventDefault();
     if (
@@ -55,113 +52,70 @@ function SignUp() {
         .catch((err) => {
           console.log(err);
         });
-// =======
-// import React, { Component } from "react";
-
-// export default class SignUp extends Component {
-//     render() {
-//         return (
-//             <div className="auth-wrapper">
-//                 <div className="auth-inner">
-//                     <form>
-//                         <h3>Sign Up</h3>
-
-//                         <div className="form-group">
-//                             <label>First name</label>
-//                             <input type="text" className="form-control" placeholder="First name" />
-//                         </div>
-
-//                         <div className="form-group">
-//                             <label>Last name</label>
-//                             <input type="text" className="form-control" placeholder="Last name" />
-//                         </div>
-
-//                         <div className="form-group">
-//                             <label>Email address</label>
-//                             <input type="email" className="form-control" placeholder="Enter email" />
-//                         </div>
-
-//                         <div className="form-group">
-//                             <label>Password</label>
-//                             <input type="password" className="form-control" placeholder="Enter password" />
-//                         </div>
-
-//                         <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
-//                         <p className="forgot-password text-right">
-//                             Already registered <a href="#">sign in?</a>
-//                         </p>
-//                     </form>
-//                 </div>
-//             </div>
-//         );
-// >>>>>>> main
     }
   }
-
   return (
-    <form>
-      <h3>Sign Up</h3>
-
-      <div className="form-group">
-        <label>First name</label>
-        <input
-          onChange={handleInputChange}
-          name="firstName"
-          type="text"
-          className="form-control"
-          value={newUser.firstName}
-          placeholder="First name"
-        />
+    <div className="auth-wrapper">
+      <div className="auth-inner">
+        <form>
+          <h3>Sign Up</h3>
+          <div className="form-group">
+            <label>First name</label>
+            <input
+              onChange={handleInputChange}
+              name="firstName"
+              type="text"
+              className="form-control"
+              value={newUser.firstName}
+              placeholder="First name"
+            />
+          </div>
+          <div className="form-group">
+            <label>Last name</label>
+            <input
+              onChange={handleInputChange}
+              name="lastName"
+              type="text"
+              className="form-control"
+              value={newUser.lastName}
+              placeholder="Last name"
+            />
+          </div>
+          <div className="form-group">
+            <label>Email address</label>
+            <input
+              onChange={handleInputChange}
+              name="email"
+              type="email"
+              className="form-control"
+              value={newUser.email}
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              onChange={handleInputChange}
+              name="password"
+              type="password"
+              className="form-control"
+              value={newUser.password}
+              placeholder="Enter password"
+            />
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary btn-block"
+            onClick={handleFormSubmit}
+          >
+            Sign Up
+          </button>
+          <p className="forgot-password text-right">
+            Already registered <a href="./sign-in">sign in?</a>
+          </p>
+        </form>
       </div>
-
-      <div className="form-group">
-        <label>Last name</label>
-        <input
-          onChange={handleInputChange}
-          name="lastName"
-          type="text"
-          className="form-control"
-          value={newUser.lastName}
-          placeholder="Last name"
-        />
-      </div>
-
-      <div className="form-group">
-        <label>Email address</label>
-        <input
-          onChange={handleInputChange}
-          name="email"
-          type="email"
-          className="form-control"
-          value={newUser.email}
-          placeholder="Enter email"
-        />
-      </div>
-
-      <div className="form-group">
-        <label>Password</label>
-        <input
-          onChange={handleInputChange}
-          name="password"
-          type="password"
-          className="form-control"
-          value={newUser.password}
-          placeholder="Enter password"
-        />
-      </div>
-
-      <button
-        type="submit"
-        className="btn btn-primary btn-block"
-        onClick={handleFormSubmit}
-      >
-        Sign Up
-      </button>
-      <p className="forgot-password text-right">
-        Already registered <a href="./sign-in">sign in?</a>
-      </p>
-    </form>
+    </div>
   );
 }
-
 export default SignUp;
