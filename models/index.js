dc7c2a6c84254761a48c5ec1694ca13479f7a2e7
erika cueva <ecueva88@gmail.com>
2021-03-07 20:35:12 -0800

    added fridge routes

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 // Commented out lines are from original copied file. After creating the api routes for this application copy the commented lines and update with appropriate information
 module.exports = {
   Food: require("./food"),
-  User: require("./user")
+  User: require("./user"),
+  Fridge: require("./fridge")
 };
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,15 +1,16 @@
 // Commented out lines are from original copied file. After creating the api routes for this application copy the commented lines and update with appropriate information
 
 const router = require("express").Router();
-// const bookRoutes = require("./books");
-const userRoutes = require("./user");
-const foodRoutes = require("./food")
 
-// Book routes
-// router.use("/books", bookRoutes);
+const userRoutes = require("./user");
+const foodRoutes = require("./food");
+const fridgeRoutes = require("./fridge")
+
 
 // User routes
 router.use("/user",userRoutes)
 // food routes
 router.use("/food",foodRoutes)
+// fridge routes
+router.use("/fridge",fridgeRoutes)
 module.exports = router;
