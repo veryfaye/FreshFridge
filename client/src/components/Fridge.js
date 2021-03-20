53b78272e763d0312acbad6ad52d87fe8418ba3b
anaiva27 <nastia.iv.91@gmail.com>
2021-03-19 20:56:41 -0700

    context added, get food/groc routes moved to Home component

--- client/src/components/Fridge.js
+++ client/src/components/Fridge.js
@@ -2,6 +2,16 @@ import React, { Component } from "react";
 // import Item from "./item"
 
 export default function Fridge() {
+    // const {
+    //     grocery: [grocItem, setGrocItem],
+    //   } = React.useContext(StoreContext);
+    
+    //   const {
+    //     list: [growingFoodList, setGrowingFoodList],
+    //   } = React.useContext(StoreContext);
+    
+    //   console.log("88888888888888groc",grocItem)
+
     return (
         <>
             <div className="auth-wrapper">
