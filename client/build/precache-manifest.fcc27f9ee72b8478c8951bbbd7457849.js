4deb43a46dd7275310539cc3db19336737578b9b
Amanda LeMoine <amanda1@mnlemoines.com>
2021-03-20 12:01:47 -0700

    build files added

---
+++ client/build/precache-manifest.fcc27f9ee72b8478c8951bbbd7457849.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "25b7a9b9929c3100fd2d",
+    "url": "/static/css/main.0c29be2d.chunk.css"
+  },
+  {
+    "revision": "25b7a9b9929c3100fd2d",
+    "url": "/static/js/main.cf124dd6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "aed3dfcc8514603c72b7",
+    "url": "/static/css/2.fcb78062.chunk.css"
+  },
+  {
+    "revision": "aed3dfcc8514603c72b7",
+    "url": "/static/js/2.cc9d6228.chunk.js"
+  },
+  {
+    "revision": "2a34c77f1adcef83f7e652489ea4a7ea",
+    "url": "/static/media/veggiestwo.2a34c77f.jpeg"
+  },
+  {
+    "revision": "118b9281ced87f7a3b4d0c2ef4737bef",
+    "url": "/index.html"
+  }
+];
