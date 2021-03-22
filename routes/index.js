bdec44637c128f1ed28a7f660d336c48385ea8fa
Amanda LeMoine <amanda1@mnlemoines.com>
2021-03-22 15:47:42 -0700

    Fixing TypeError: path must be absolute or specify root to res.sendFile

--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,4 @@ const apiRoutes = require("./api");
 // API Routes
 router.use("/api", apiRoutes);
 
-// If no API routes are hit, send the React app
-// router.use(function(req, res) {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
-
-// router.get("./home",(req,res,next)=>{
-//     console.log("home accessed")
-//     console.log(req.user)
-//     res.json({
-//         message: 'You made it to the secure route',
-//         user: req.user,
-//         token: req.query.secret_token
-//       })
-// })
-
 module.exports = router;
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join("client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
 }
 
