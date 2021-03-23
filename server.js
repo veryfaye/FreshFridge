b74e9c202986e78f70b7b7668e8848ebc94b4c29
Amanda LeMoine <amanda1@mnlemoines.com>
2021-03-23 16:28:40 -0700

    Server.js updated for heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
-// const path = require("path");
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const routes = require("./routes");
@@ -15,7 +15,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join("client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
 }
 
